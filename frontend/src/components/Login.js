dae2df500bffdb2d39799eef9da4117de92b0d01
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-11-02 21:10:31 +0300

    Styling for the login and register

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './css/Login.css'; // Import your custom CSS
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -32,14 +33,14 @@ function Login() {
       if (response.status === 200) {
         const result = await response.json();
         localStorage.setItem('access_token', result.access_token);
-        toast.success('Login successful'); // Show success notification
+        toast.success('Login successful');
         navigate('/');
       } else {
-        toast.error('Invalid username or password'); // Show error notification
+        toast.error('Invalid username or password');
       }
     } catch (error) {
       console.error('Error logging in:', error);
-      toast.error('An error occurred during login'); // Show error notification
+      toast.error('An error occurred during login');
     }
   };
 
@@ -49,26 +50,30 @@ function Login() {
   };
 
   return (
-    <div>
+    <div className="form-container">
       <h1>Login</h1>
       <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
         <button type="submit">Login</button>
       </form>
-      <ToastContainer /> {/* This component renders the notifications */}
+      <ToastContainer />
     </div>
   );
 }
